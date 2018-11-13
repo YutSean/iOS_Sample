ff3189006a4a9755be91eedf1c52bb1bbaf0da1f
Yutong <yutongsean@qq.com>
2018-11-13 14:04:17 +0100

    Initial Commit

---
+++ Sample_Code/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample_Code
+//
+//  Created by Yutong on 2018/11/13.
+//  Copyright © 2018 Yutong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample_Code/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample_Code
+//
+//  Created by Yutong on 2018/11/13.
+//  Copyright © 2018 Yutong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
